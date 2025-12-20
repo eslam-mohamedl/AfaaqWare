765ba4fd338ce36ed159140a3acbd6f864bd787d
eslam-mohamedl <ee.eslam355@gmail.com>
2025-12-20 09:52:39 +0200

    feat(aboutSection): add section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,12 +2,14 @@ import Header from "@/components/organisms/Header";
 import Footer from "@/components/organisms/Footer";
 import Services from "@/components/organisms/Services";
 import ContractSection from "@/components/organisms/ContractSection";
+import AboutAfaaqHome from "@/components/organisms/AboutAfaaqHome";
 function Page() {
   return (
     <div className="pt-20">
       <Header />
       <Services />
       <ContractSection />
+      <AboutAfaaqHome />
       <Footer />
     </div>
   );
