2296f470198413a6787f6fb85a986a5b52db9e5f
eslam-mohamedl <ee.eslam355@gmail.com>
2025-12-21 10:35:38 +0200

    update

--- src/components/organisms/Services.tsx
+++ src/components/organisms/Services.tsx
@@ -1,15 +1,12 @@
 import { dataServices } from "@/utils/data";
 import Card from "../molecules/Card";
 import TitleSection from "../molecules/TitleSection";
-
+import { useTranslations } from "next-intl";
 export default function Services() {
+  const t = useTranslations("servicesSection.heading");
   return (
     <section className="mb-20">
-      <TitleSection
-        title="Our Services"
-        text="We offer you a  comprehensive range of digital services."
-        isBlue={true}
-      />
+      <TitleSection title={t("title")} text={t("subTitle")} isBlue={true} />
       <div className="container grid grid-cols-1 md:grid-cols-4 gap-3 ">
         {dataServices.map((card) => (
           <Card
